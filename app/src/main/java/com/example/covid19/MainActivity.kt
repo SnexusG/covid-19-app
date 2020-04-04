29f469a12619af6028cccd3aaa10fd692954ac18
Advait Bansode <advaitbansode4@gmail.com>
2020-04-04 22:53:48 +0530

    Added SymptomTest activity(Java) and a button to start that activity

--- app/src/main/java/com/example/covid19/MainActivity.kt
+++ app/src/main/java/com/example/covid19/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.covid19
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        QuizButton.setOnClickListener {
+            test()
+        }
+    }
+
+    fun test(){
+        val intent = Intent(this, SymptomTest::class.java)
+        startActivity(intent)
     }
 }
