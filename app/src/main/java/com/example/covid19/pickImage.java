202f00316d4e9b6afcd47acb5b97d7bfc27ad680
SNexusG <swapnilgore029@gmail.com>
2020-04-07 20:47:13 +0530

    pickImage + web scraping for news

---
+++ app/src/main/java/com/example/covid19/pickImage.java
@@ -0,0 +1,84 @@
+package com.example.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+public class pickImage extends AppCompatActivity {
+
+    private ImageView MyimageView;
+    private Button imgBtn;
+    private Uri imaageUri;
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_image);
+
+        MyimageView = findViewById(R.id.image_view);
+        imgBtn = findViewById(R.id.choose_img);
+
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                        String[] Permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        requestPermissions(Permissions, PERMISSION_CODE);
+                    }else{
+                        pickImageFromGallery();
+                    }
+                }else{
+                    pickImageFromGallery();
+                }
+            }
+        });
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+    public void pickImageFromGallery(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch ((requestCode)){
+            case PERMISSION_CODE :
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery();
+                }else{
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            imaageUri = data.getData();
+            Picasso.get().load(imaageUri).into(MyimageView);
+            //MyimageView.setImageURI(data.getData());
+        }
+    }
+
+}
--- app/src/main/java/com/example/covid19/statsTemp.java
+++ app/src/main/java/com/example/covid19/statsTemp.java
@@ -16,7 +16,8 @@ import java.io.IOException;
 public class statsTemp extends AppCompatActivity {
 
     //state-wise stats
-    private String[][] stats = new String[30][4];
+    private String[][] stats = new String[31][4];
+    private String[] news = new String[5];
     private String totalCount;
 
     @Override
@@ -27,6 +28,51 @@ public class statsTemp extends AppCompatActivity {
             Content content = new Content();
             content.execute();
 
+            Content2 content2 = new Content2();
+            content2.execute();
+
+    }
+
+    private class Content2 extends AsyncTask<Void, Void, Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            System.out.println("NEWS");
+            for(String i : news){
+                System.out.println(i);
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String url = "https://www.moneycontrol.com/news/india/coronavirus-india-news-live-updates-covid-19-pandemic-us-delhi-maharashtra-kerala-statewise-tally-death-toll-lockdown-news-5114201.html";
+            try {
+                Document doc = Jsoup.connect(url).get();
+                Elements data = doc.select("ul.live-blog");
+
+                for(int i = 0; i < 5; i++) {
+                    String text = data.select("li")
+                            .eq(i)
+                            .select("p")
+                            .text();
+                    int size = data.size();
+                    news[i] = text;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
     private class Content extends AsyncTask<Void, Void, Void>{
@@ -38,7 +84,7 @@ public class statsTemp extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            for(int i = 0; i < 30; i++){
+            for(int i = 0; i < 31; i++){
                 for(int j = 0; j < 4; j++){
                     System.out.print(stats[i][j]+ " ");
                 }
@@ -60,7 +106,7 @@ public class statsTemp extends AppCompatActivity {
                 Elements data = doc.select("div.col-xs-12");
                 int size = data.size();
 
-             for(int i = 0; i < 30; i++) {
+             for(int i = 0; i < 31; i++) {
                  for(int j = 1; j < 5; j++) {
                      String text = data.select("tbody")
                              .select("tr")
@@ -74,7 +120,7 @@ public class statsTemp extends AppCompatActivity {
 
              totalCount = data.select("tbody")
                      .select("tr")
-                     .eq(30)
+                     .eq(31)
                      .select("td")
                      .eq(1)
                      .text();
